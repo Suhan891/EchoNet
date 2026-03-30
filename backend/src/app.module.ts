a41c08bb1bae350c3364bfcfb17333c502dbf996
Suhan Patra <patra.suhan2006@gmail.com>
2026-03-30 18:21:58 +0530

    Creation of docker compose for usage of Redis on Bull Mq for asynchronous response and Installation and initialisations of them in the App module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,6 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGaurd } from './common/gaurds/jwt-auth';
 import { ProfileGaurd } from './profile/gaurds/profile.gaurd';
 import { ConfigModule } from '@nestjs/config';
 import { FollowModule } from './follow/follow.module';
@@ -16,10 +15,18 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { Request } from 'express';
 import { authUserDto } from './auth/tokens/token.dto';
 import { UserThrottlerGaurd } from './common/gaurds/throtler.gaurd';
+import { BullModule } from '@nestjs/bullmq';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
     PrismaModule,
     AuthModule,
     ProfileModule,
@@ -38,14 +45,11 @@ import { UserThrottlerGaurd } from './common/gaurds/throtler.gaurd';
         },
       },
     ]),
+    EventEmitterModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGaurd,
-    },
     {
       provide: APP_GUARD,
       useClass: ProfileGaurd,
