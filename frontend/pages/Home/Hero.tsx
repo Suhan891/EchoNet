3904f83a010eeed490bbb7778317b7ec8ea5961b
Suhan Patra <patra.suhan2006@gmail.com>
2026-04-05 15:30:20 +0530

    Initaial Landing page setup for the project

---
+++ frontend/pages/Home/Hero.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+
+export default function Hero() {
+  return (
+    <div className='mx-auto min-h-full flex-col items-center'>
+    <Card size="default" className="mx-auto my-16 w-full max-w-3xl" >
+      <CardHeader>
+        <CardTitle>Landing Page</CardTitle>
+        <CardDescription>
+          This would be built later
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>
+          This would contain a brief summary of the entire project
+        </p>
+      </CardContent>
+      <CardFooter>
+        <Button variant="outline" size="icon-lg" className="w-full">
+          Comming Soon
+        </Button>
+      </CardFooter>
+    </Card>
+    </div>
+  )
+}
+
---
+++ frontend/pages/Home/Navbar.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+
+const Navbar = () => {
+  const { setTheme } = useTheme();
+  return (
+    <nav className="px-4 flex items-center justify-between sticky top-0 bg-background z-10 text-3xl">
+      <div className="font-bold">
+        <Link href={"/"}>EchoNet</Link>
+      </div>
+      <div>
+        <Button variant={"ghost"} className="text-xl">Summary</Button>
+      </div>
+      <div className="flex gap-3">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button
+          className="px-12 rounded-2xl"
+          variant={"default"}
+          size={"icon-lg"}
+        >
+          Get Started
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
