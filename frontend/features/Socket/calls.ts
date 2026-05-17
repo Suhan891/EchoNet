96c34d417ef3ccd4f364d5f88be35aee446d1eae
Suhan Patra <patra.suhan2006@gmail.com>
2026-05-17 10:12:45 +0530

    Made the socket connection on frontend , based on data be send if user is active known from redis

--- frontend/features/Socket/calls.ts
+++ frontend/features/Socket/calls.ts
@@ -3,6 +3,7 @@ import { useUserStore } from "@/stores/UserStore";
 import { queryKeys } from "@/utils/query.key";
 import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
+import { toast } from "sonner";
 
 export function useCalls() {
   const socket = useUserStore((state) => state.socket);
@@ -10,10 +11,27 @@ export function useCalls() {
 
   useEffect(() => {
     if (!socket) return;
-    socket.on("notification", (purpose: "STORY" | "CHAT" | "MESSAGE" | "REEL" | "POST" | "COMMENT" | undefined) => {
-      queryClient.invalidateQueries({ queryKey: [queryKeys.NOTIFICATIONS] });
-      if(purpose === 'CHAT' || purpose === 'MESSAGE')
-        queryClient.invalidateQueries({ queryKey: [queryKeys.CHAT] });
+    socket.on(
+      "notification",
+      (
+        purpose:
+          | "STORY"
+          | "CHAT"
+          | "MESSAGE"
+          | "REEL"
+          | "POST"
+          | "COMMENT"
+          | undefined,
+      ) => {
+        queryClient.invalidateQueries({ queryKey: [queryKeys.NOTIFICATIONS] });
+        if (purpose === "CHAT")
+          queryClient.invalidateQueries({ queryKey: [queryKeys.CHAT] });
+      },
+    );
+
+    socket.on("message", ({ chatId, content }) => {
+      queryClient.invalidateQueries({ queryKey: [chatId] });
+      toast.success(content);
     });
     return () => {
       socket.off("notification");
--- frontend/features/Socket/starter.ts
+++ frontend/features/Socket/starter.ts
@@ -1,46 +1,49 @@
 "use client";
-import { useProfileStore } from "@/stores/ProfileStore";
-import { useUserStore } from "@/stores/UserStore";
 import Cookie from "js-cookie";
 import { useEffect, useRef } from "react";
 import { io, Socket } from "socket.io-client";
+import { useAvailProfiles } from "./initial";
+import { useUserStore } from "@/stores/UserStore";
+import { useCalls } from "./calls";
 
 export function useSocket() {
   const token = Cookie.get("accessToken");
   const profileId = Cookie.get("profile");
-  const userId = useUserStore((state) => state.userId);
   const socketRef = useRef<Socket | null>(null);
-  const tokenRef = useRef<string | null>(token);
+
+  useAvailProfiles();
+  useCalls();
 
   useEffect(() => {
-    tokenRef.current = token;
-  }, [token]);
-
-  useEffect(() => {
-    const availToken = tokenRef.current;
-    if (!availToken || !profileId) return;
+    if (!token || !profileId) return;
     socketRef.current = io(process.env.NEXT_PUBLIC_REQUEST_APP_URL, {
       auth: {
-        token: `Bearer ${availToken}`,
+        token: `Bearer ${token}`,
         profileId,
       },
     });
 
-    useProfileStore.getState().setSocket(socketRef.current);
+    // socketRef.current.on('connect_error', (error) => {
+    //   console.error('Socket connect_error:', error);
+    //   if (
+    //     error?.message === 'Invalid token' ||
+    //     error?.message === 'Token has expired' ||
+    //     error?.message === 'All conditions not satisfied'
+    //   ) {
+    //     Cookie.remove('accessToken');
+    //     Cookie.remove('profile');
+    //     window.location.href = '/login';
+    //   }
+    // });
+
+    useUserStore.getState().setSocket(socketRef.current);
+
     return () => {
       if (socketRef.current) {
+        //socketRef.current.off('connect_error');
         socketRef.current.disconnect();
         socketRef.current = null;
       }
     };
-  }, [profileId, userId]);
-
-  useEffect(() => {
-    if (!socketRef.current || !token) return;
-    socketRef.current.auth = {
-      ...socketRef.current.auth,
-      token,
-    };
-    useProfileStore.getState().setSocket(socketRef.current);
-  }, [token]);
+  }, [profileId, token]);
 }
