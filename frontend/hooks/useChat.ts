815fb13d54a53b299ea9b9aa7705521ecdf128c2
Suhan Patra <patra.suhan2006@gmail.com>
2026-05-17 10:13:59 +0530

    Made all the calling routes for chat like viewing members if it is a group else routing to that member if private

--- frontend/hooks/useChat.ts
+++ frontend/hooks/useChat.ts
@@ -1,14 +1,24 @@
 import {
   AddMessage,
+  AddProfileGroup,
   CreateGroup,
   CreatePrivate,
   GetAllChatMsgs,
   GetChats,
+  GetMembersGroup,
   getProfForGroup,
+  GetProfForGroupAdd,
   getProfForPrivate,
   UpdateApproval,
+  ViewMessage,
 } from "@/service/chat";
-import { ChatDto, ChatsMsgsDto, CreateChatDto } from "@/types/chat";
+import {
+  ChatDto,
+  ChatMembersDto,
+  ChatsMsgsDto,
+  CreateChatDto,
+  MessageViewDto,
+} from "@/types/chat";
 import { ErrorResponse, SuccessResponse } from "@/types/common";
 import { queryKeys } from "@/utils/query.key";
 import { messageType } from "@/validations/chats/message";
@@ -26,6 +36,30 @@ export function useAllProfileForGroup() {
     queryFn: () => getProfForGroup(),
   });
 }
+export function useGroupMembers(chatId: string, work: "VIEW" | "ADD") {
+  return useQuery<SuccessResponse<ChatMembersDto>, ErrorResponse>({
+    queryKey: [chatId, work],
+    queryFn: () => GetMembersGroup(chatId),
+    enabled: work === "VIEW",
+  });
+}
+export function useProfileGroupAdd(chatId: string, work: "VIEW" | "ADD") {
+  return useQuery<SuccessResponse<CreateChatDto[]>, ErrorResponse>({
+    queryKey: [chatId, work],
+    queryFn: () => GetProfForGroupAdd(chatId),
+    enabled: work === "ADD",
+  });
+}
+export function useAddMember() {
+  return useMutation<
+    SuccessResponse<null>,
+    ErrorResponse,
+    { chatId: string; name: string }
+  >({
+    mutationFn: (payload) => AddProfileGroup(payload),
+  });
+}
+
 export function useCreatePrivate() {
   return useMutation<SuccessResponse<null>, ErrorResponse, string>({
     mutationFn: (payload) => CreatePrivate(payload),
@@ -49,7 +83,7 @@ export function useChatApproval() {
 }
 export function useAllChatMsgs(chatId: string) {
   return useQuery<SuccessResponse<ChatsMsgsDto>, ErrorResponse>({
-    queryKey: [queryKeys.CHAT, chatId],
+    queryKey: [chatId],
     queryFn: () => GetAllChatMsgs(chatId),
     enabled: !!chatId,
   });
@@ -58,5 +92,13 @@ export function useAllChatMsgs(chatId: string) {
 export function useAddMessage() {
   return useMutation<SuccessResponse<null>, ErrorResponse, messageType>({
     mutationFn: (payload) => AddMessage(payload),
+  });
+}
+
+export function useViewMsg(msgId: string, count: number) {
+  return useQuery<SuccessResponse<MessageViewDto>, ErrorResponse>({
+    queryKey: [msgId],
+    queryFn: () => ViewMessage(msgId),
+    enabled: count !== 0,
   });
 }
--- frontend/service/chat.ts
+++ frontend/service/chat.ts
@@ -2,39 +2,63 @@ import { messageType } from "@/validations/chats/message";
 import { Request } from "./common/requests";
 
 async function postJson(path: string, payload: unknown) {
-  return Request(`/chat/${path}`, { method: 'POST', body: payload});
+  return Request(`/chat/${path}`, { method: "POST", body: payload });
 }
 async function getJson(path: string) {
-  return Request(`/chat/${path}`, { method: 'GET', body: undefined});
+  return Request(`/chat/${path}`, { method: "GET", body: undefined });
 }
 async function putJson(path: string, payload: unknown) {
   return Request(`/chat/${path}`, { method: "PUT", body: payload });
 }
 export async function getProfForPrivate() {
-    return await getJson('private')
+  return await getJson("private");
 }
 export async function getProfForGroup() {
-    return await getJson('group')
+  return await getJson("group");
+}
+
+
+export async function GetMembersGroup(chatId: string) {
+  return getJson(`group/${chatId}/memb`);
+}
+export async function GetProfForGroupAdd(chatId: string) {
+  return getJson(`group/${chatId}`);
 }
 
 export async function CreatePrivate(name: string) {
-  return postJson(`private?profile=${name}`, undefined)
+  return postJson(`private?profile=${name}`, undefined);
 }
-export async function CreateGroup(data:FormData) {
-  return postJson(`group/create`, data)
+export async function CreateGroup(data: FormData) {
+  return postJson(`group/create`, data);
+}
+
+export async function AddProfileGroup(payload: {
+  chatId: string;
+  name: string;
+}) {
+  return putJson(
+    `group/${payload.chatId}?profileId=${payload.name}`,
+    undefined,
+  );
 }
 
 export async function GetChats() {
-  return getJson(``)
+  return getJson(``);
 }
 
 export async function UpdateApproval(chatId: string) {
-  return putJson(`approve/${chatId}`, undefined)
+  return putJson(`approve/${chatId}`, undefined);
 }
-export async function GetAllChatMsgs(chatId:string) {
-  return getJson(`messages/${chatId}`)
+export async function GetAllChatMsgs(chatId: string) {
+  return getJson(`messages/${chatId}`);
 }
 
 export async function AddMessage(payload: messageType) {
-  return postJson(`message/${payload.chatId}`,{format:  payload.format, content: payload.content})
+  return postJson(`message/${payload.chatId}`, {
+    format: payload.format,
+    content: payload.content,
+  });
+}
+export async function ViewMessage(msgId: string) {
+  return getJson(`message/view/${msgId}`);
 }
